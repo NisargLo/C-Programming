633f520631e6bb8c7b82a2b32a89bc0b13e0244b
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-08-22 19:09:44 +0530

    Update L5-4.c

--- L5-4.c
+++ L5-4.c
@@ -1,9 +1,19 @@
-include<stdio.h>
+#include<stdio.h>
 void main(){
      int a,b,c;
      printf("Enter a,b,c");
      scanf("%d",&a);
      scanf("%d",&b);
      scanf("%d",&c);
-      if(a>B)
+      if(a>b){
+      }
+      else if(b>c){
+            printf("B is largest");
+      }
+      else if(a>c){
+            printf("A is largest");
+      }
+      else{
+            printf("C is largest");
+      }
 }
