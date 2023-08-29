5e147a99457ec2c35f2a2daff230da2fba10c596
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-08-29 23:20:12 +0530

    29-8-23

---
+++ Lab 6/L6-A-5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main() {
+	int a,b,c,d,e;
+	float f;
+	printf("Enter Marks of your 5 Subjects : ");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	scanf("%d",&d);
+	scanf("%d",&e);
+	f=(a+b+c+d+e)/5;
+	printf("%f",f);
+	if(f<35)
+	{
+	printf("\nFail");
+	}
+	else if(f>=35 && f<=45)
+	{
+	printf("\nPass Class");
+	}
+	else if(f>=46 && f<=60)
+	{
+	printf("\nSecond Class");
+	}
+	else if(f>=61 && f<=70)
+	{
+	printf("\nFirst Class");
+	}
+	else{printf("\nDistinction");
+	}
+}
