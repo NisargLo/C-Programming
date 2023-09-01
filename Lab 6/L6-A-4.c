0dfd9a6c01cdcd9b5bae17d95cb440453e3e08d3
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-09-01 20:01:08 +0530

    1-9-23

---
+++ Lab 6/L6-A-4.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main() {
+     int n;
+     printf("Enter an Integral NO. : ");
+     scanf("%d",&n);
+     (n%2==0)?(printf("Last digit of NO. is Even")):(printf("Last digit of NO. is Odd"));
+}
+
---
+++ Lab 6/L6-C-1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main() {
+     int a,b,c;
+     printf("Enter 3 Numbers : ");
+     scanf("%d %d %d",&a,&b,&c);
+if(a>b){
+     if(b>c){
+          printf("Second largest NO. is %d",b);
+     }
+     else if(a>c){
+          printf("Second largest NO. is %d",c);
+     }
+     else{
+          printf("Second largest NO. is %d",a);
+     }
+}
+else if(a>c){
+     printf("Second largest NO. is %d",a);
+}
+else{
+     if(b>c){
+           printf("Second largest NO. is %d",c);
+     }
+     else{
+     printf("Second largest NO. is %d",b);
+          }
+     }
+}
