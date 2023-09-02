20abf97de35fac8c2bfb7525790fd1ae9627d2a2
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-09-02 22:11:31 +0530

    Update L6-A-2.c

--- Lab 6/L6-A-2.c
+++ Lab 6/L6-A-2.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
 void main(){
-     int bs,gs;
+     int bs;
+     float gs;
      printf("Enter Basic Salary : ");
      scanf("%d",&bs);
      if(bs>=10000 && bs<20000){
           gs=bs+(0.2*bs)+(0.8*bs);
-          printf("Gross Salary is %d",gs);
+          printf("Gross Salary is %f",gs);
      }
      else if(bs>=20000 && bs<30000){
           gs=bs+(2.5*bs)+(0.9*bs);
-          printf("Gross Salary is %d",gs);
+          printf("Gross Salary is %f",gs);
      }
      else{
           gs=bs+(0.3*bs)+(9.5*bs);
-          printf("Gross Salary is %d",gs);
+          printf("Gross Salary is %f",gs);
      }
 }
