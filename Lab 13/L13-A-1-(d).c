9f3f8d556d9aff1e12f46e08548de7ffba26abb1
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-10-05 23:05:54 +0530

    Update L13-A-1-(d).c

--- Lab 13/L13-A-1-(d).c
+++ Lab 13/L13-A-1-(d).c
@@ -1,8 +1,8 @@
 /*           1
-           2   2
-         3   3   3
-       4   4   4   4
-     5   5   5   5   5
+           1   2
+         1   2   3
+       1   2   3   4
+     1   2   3   4   5
 */
 
 #include<stdio.h>
@@ -14,7 +14,7 @@ void main() {
                printf(" ");
           }
           for(k=1;k<=j;k++){
-               printf("%d ",i-1);
+               printf("%d ",k);
           }
           printf("\n");
           }
