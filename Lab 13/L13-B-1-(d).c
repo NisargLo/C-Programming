2026d3a1ded747c204e83a98c776cd523ee9782d
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-10-06 06:40:16 +0530

    6-10-23

--- Lab 13/L13-B-1-(d).c
+++ Lab 13/L13-B-1-(d).c
@@ -1,8 +1,8 @@
-/*           1
-           A   B
-         1   2   3
-       C   D   E   F
-     1   2   3   4   5
+/*       1
+        A B
+       1 2 3
+      C D E F
+     1 2 3 4 5
 */
 
 #include<stdio.h>
