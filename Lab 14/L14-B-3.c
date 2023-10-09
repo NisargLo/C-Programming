1d50398bf67b5969dfd78d590d45cf7140af846e
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-10-09 18:58:29 +0530

    Update L14-B-3.c

--- Lab 14/L14-B-3.c
+++ Lab 14/L14-B-3.c
@@ -2,32 +2,26 @@
 
 #include<stdio.h>
 void main(){
-	int n,i,j,min,c;
+	int n,i,j,temp;
 	printf("How many NO. are you entering ? ");
 	scanf("%d",&n);
 	int a[n],b[n];
-	for(i=0;i<n;i++) {
+	for(i=0;i<n;i++){
 		printf("Enter NO. %d : ",i+1);
 		scanf("%d",&a[i]);
 	}
-	for(j=0;j<n;j++){
-		c=a[0];
-	for(i=0;i<n;i++){
-		if(a[i]<c){
-			min=a[i];
-		}
-		else{
-			min=c;
-		}
-		c=min;
-	}
-	b[j]=min;
-	}
-
+	 for(i=0;i<n;i++){
+        for(j=i+1;j<n;j++){
+            if(a[i]>a[j]){
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
 	printf("Your NO. in Ascending Order are :\n");
 	for(i=0;i<n-1;i++){
-		printf("%d, ",b[i]);
+		printf("%d, ",a[i]);
 	}
-	printf("%d",b[n-1]);
+	printf("%d",a[n-1]);
 }
-
