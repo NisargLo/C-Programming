6f6b7c5159dba275595dbf2a9a8bd6baba590803
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-11-15 23:54:24 +0530

    Update L21-B-2.c

--- Lab 21/L21-B-2.c
+++ Lab 21/L21-B-2.c
@@ -9,19 +9,10 @@ if(f==NULL){
      printf("Error is opening File\n");
 }
 else{
-     fseek(f,0,SEEK_END);
-     l=ftell(f);
-     char c[l];
-     fseek(f,0,SEEK_SET);
-     for(i=0;i<l;i++){
-          c[i]=fscanf(f,"%c",&c[i]);
-          if(c[i-1]==' ' || c[i-1]=='\n' || i==0){
-               if(c[i]>='a' || c[i]<='z'){
-                    c[i]-=32;
-               }
-          }
-          printf("%c",c[i]);
-     }
+     
+
+
+     
      fclose(f);
 }
 }
