128526a1f02b9ccc40ed4d87c646638d274247aa
Nisarg Lo <140806157+NisargLo@users.noreply.github.com>
2023-12-15 15:00:04 +0530

    Update L12-B-3.c

--- Lab 12/L12-B-3.c
+++ Lab 12/L12-B-3.c
@@ -1,16 +1,34 @@
-//Print Pascal triangle.
+/*   Print Pascal triangle.
+
+	1
+	1  1
+	1  2  1
+	1  3  3  1
+	1  4  6  4  1
+	1  5 10 10  5  1
+	and so on...
+*/
 
 #include<stdio.h>
 void main() {
-	int l,s,n;
+	int i,j,s,c=1,n;
+/*	c - coef
+	s - space
+	n - NO. of lines
+*/
 	printf("Enter NO. : ");
 	scanf("%d",&n);
-	for(l=1;l<=n;l++){
-		for(s=1;s<=l+1;s++){
-			printf(" ");
-			  
-		
-		
+	printf("%4d\n",c);
+	for(i=1;i<=n;i++){
+		for(j=0;j<=i;j++){
+			if(j==0 || i==0){
+				c=1;
+			}
+			else{
+				c=c*(i-j+1)/j;
+			}
+			printf("%4d",c);
 		}
+	  	printf("\n");
 	}
 }
